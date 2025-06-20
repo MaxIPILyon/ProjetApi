58bae605db7da9925c217770a82dcc21366a738d
JeanBonnevie <bonnevie.jean7@gmail.com>
2025-06-20 14:37:48 +0200

    correctif mot de passe optionel

--- controllers/userApiController.js
+++ controllers/userApiController.js
@@ -38,21 +38,25 @@ module.exports.createUser = async (req, res) => {
 }
 
 module.exports.updateUser = async (req, res) => {
-    try {
-        let salt = await bcrypt.genSalt(10);
-        req.body.password = await bcrypt.hash(req.body.password, salt);
-        
-                // req.body.password = crypto.createHmac("sha512", process.env.SECRET_KEY)
-                // .update(req.body.password)
-                // .digest("base64");
-        
-                data = await userApiService.updateUser({_id: req.params.id}, req.body)
-                return res.status(200).json({status: 200, data: data, message: "Succesfully Users Updated"})
+  try {
+    // Si un mot de passe est fourni, on le hash
+    if (req.body.password && req.body.password.trim() !== "") {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    } else {
+      // Si pas de mot de passe, on ne met pas à jour ce champ
+      delete req.body.password;
     }
-    catch(e) {
-        return res.status(400).json({status: 400, message: e.message});
-    }
-}
+
+    const data = await userApiService.updateUser({ _id: req.params.id }, req.body);
+
+    return res
+      .status(200)
+      .json({ status: 200, data: data, message: "Succesfully Users Updated" });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
 
 module.exports.deleteUser = async (req, res) => {
     try {      
